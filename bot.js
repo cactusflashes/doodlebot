30129ef9f5162f92b153b8894ba9788fef646cf1
cactusflashes <121457318+cactusflashes@users.noreply.github.com>
2023-09-21 20:39:22 -0700

    organization

--- bot.js
+++ bot.js
@@ -5,51 +5,48 @@ import { Client, Events, GatewayIntentBits, ActivityType } from 'discord.js';
 const token = process.env.DISCORD_TOKEN; 
 const channelId = process.env.CHANNEL_ID;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY
+});
 
 dotenv.config();
-
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-		client.user.setStatus('online');
-		client.user.setActivity(`you sleep`, { type: ActivityType.Watching });
+console.log(`Ready! Logged in as ${c.user.tag}`);
+client.user.setStatus('online');
+lient.user.setActivity(`you sleep`, { type: ActivityType.Watching });
+			  
+async function main() {
+	const channelGen = client.channels.cache.get(channelId);
+	const completion = await openai.chat.completions.create({
+	messages: [{ role: 'user', content: 'Give me a creative prompt for an artist or creative to capture, that is no longer than three sentences and under 100 words.' }],
+	model: 'gpt-3.5-turbo',
+});		
 
+const response = completion.choices[0].message.content; 
 
-		const openai = new OpenAI({
-			apiKey: process.env.OPENAI_API_KEY
-			});
-				  
-		async function main() {
-			const channelGen = client.channels.cache.get(channelId);
-			const completion = await openai.chat.completions.create({
-				messages: [{ role: 'user', content: 'Give me a creative prompt for an artist or creative to capture, that is no longer than three sentences and under 100 words.' }],
-				model: 'gpt-3.5-turbo',
-			});		
+function getGreeting() {
+	let currentHour = new Date().getHours();	
+		if (currentHour >= 5 && currentHour < 12) {
+		return "Good morning,"; //if the time is past 5am and before noon => Good Morning 
+		} else if (currentHour >= 12 && currentHour < 18) {
+		return "Good afternoon,"; //if the time is past noon but before 6pm => Good Afternoon
+		} else if (currentHour >= 18 && currentHour < 21) {
+		return "Good evening,"; //if the time is past 6pm but before 9pm => Good Evening
+		} else {
+		return "Good night,"; //anything past 9pm before 5am => Good Night 
+		}
+	}
 
-			const response = completion.choices[0].message.content; 
+let greeting = getGreeting(); 
 
-			function getGreeting() {
-				let currentHour = new Date().getHours();	
-					if (currentHour >= 5 && currentHour < 12) {
-					return "Good morning,"; //if the time is past 5am and before noon => Good Morning 
-					} else if (currentHour >= 12 && currentHour < 18) {
-					return "Good afternoon,"; //if the time is past noon but before 6pm => Good Afternoon
-					} else if (currentHour >= 18 && currentHour < 21) {
-					return "Good evening,"; //if the time is past 6pm but before 9pm => Good Evening
-					} else {
-					return "Good night,"; //anything past 9pm before 5am => Good Night 
-					}
-				}
+channelGen.send(`${greeting} @everyone! It's time to strech those creative muscles. Your prompt of the day is as follows: **${response}**`);
 
-				let greeting = getGreeting(); 
+}	  
 
-				channelGen.send(`${greeting} @everyone! It's time to strech those creative muscles. Your prompt of the day is as follows: **${response}**`);
+main();		
 
-		}	  
-
-		main();		
-
-	});
+});
 	
-	client.login(token);
+client.login(token);
 
 	
